748412d9892cf6986b2b2940253026e662f8af19
Alejandro Henestroza <alehenestroza@gmail.com>
2021-03-10 22:03:39 -0300

    Simplified Search logic to use less return statements

--- entry_search.py
+++ entry_search.py
@@ -10,9 +10,8 @@ def search_entry(text_to_match):
                 return data[text_to_match]
             else:
                 messagebox.showwarning(title="No matches", message="No match could be found for your search term.")
-                return None
     except FileNotFoundError:
         messagebox.showerror(title="Oops", message="There is no data file.\n"
                                                    "This may be because it's the first time you use this program.\n"
                                                    "Try saving some entries before using the search button.")
-        return None
+    return None
